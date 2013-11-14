0b78a5316decc2d9b5a6b826f47e0b009b0ca7dd
Greg Slodkowicz <jergosh@gmail.com>
2013-11-14 18:00:01 +0000

    Fixes in prank_bsub.py

--- bin/prank_bsub.py
+++ bin/prank_bsub.py
@@ -4,18 +4,20 @@ from os import path
 
 prank_cmd = "prank -d={} -t={} -o={} -prunetree"
 
-indir = "data/ens/73/tmp/"
+indir = "data/ens/73/tmp"
 treedir = "data/ens/73/seqsets"
 alndir = "data/ens/73/aln"
 
-# for infile in glob(path.join(indir, "*.fa")):
-for infile in glob(path.join(indir, "*.fa"))[:2]:
-    basename = path.basename(infile).partition('.')[0]
+clades = [ "Eutheria" ]
+for clade in clades:
+    # for infile in glob(path.join(indir, "*.fa")):
+    for infile in glob(path.join(indir, clade, "*.fa"))[:2]:
+        basename = path.basename(infile).partition('.')[0]
 
-    treefile = path.join(treedir, basename + '.nh')
-    outfile = path.join(alndir, basename + '_prank')
+        treefile = path.join(treedir, clade, basename + '.nh')
+        outfile = path.join(alndir, clade, basename + '_prank')
 
-    prank = prank_cmd.format(infile, treefile, outfile)
-
-    p = Popen('bsub', '-o /deb/null', prank)
-    p.wait()
+        prank = prank_cmd.format(infile, treefile, outfile)
+        print prank
+        p = Popen('bsub', '-o /deb/null', prank)
+        p.wait()
