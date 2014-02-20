1080bd7924543abfd58e4a52ff3b16688f3775e0
Greg Slodkowicz <gregs@ebi.ac.uk>
2014-02-20 13:17:52 +0000

    Moved some code to utils.py

---
+++ bin/utils.py
@@ -0,0 +1,31 @@
+import os
+from os import path
+
+def check_dir(dirname):
+    if not path.exists(dirname):
+        os.mkdir(dirname)
+
+# Ensembl REST API stuff
+http = httplib2.Http(".cache")
+
+server = "http://beta.rest.ensembl.org/"
+def ens_get(ext, *args, **kwargs):
+    if len(args):
+        ext += '&'.join([ urllib.quote(a) for a in args]) 
+        
+    if len(kwargs):
+        ext += '&' + urllib.urlencode(kwargs)
+
+    print ext
+
+    resp, content = http.request(server+ext, method="GET", headers={"Content-Type":"application/json"})
+    
+    if not resp.status == 200:
+        exc = IOError(resp)
+        exc.errno = resp
+        raise exc
+
+    decoded = json.loads(content)
+    return decoded
+
+
