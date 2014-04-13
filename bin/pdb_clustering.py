6074667c9d53c9b32cc360f890abac6a27c877d2
Greg Slodkowicz <gregs@ebi.ac.uk>
2014-04-13 09:50:30 +0100

    Added a script for clustering residues under positive selection.

---
+++ bin/pdb_clustering.py
@@ -0,0 +1,87 @@
+import re
+import sys
+import glob
+from os import path
+from argparse import ArgumentParser
+import utils
+
+from Bio import SeqIO
+from Bio import AlignIO
+from Bio import SeqUtils
+from Bio import PDB
+
+import numpy as np
+from sklearn.cluster import AffinityPropagation
+
+import brewer2mpl
+
+re_resid = re.compile("(-?[0-9]+)([A-Z]*)")
+
+def parse_coord(coord):
+    n, ic = re_resid.match(coord).groups()
+    if ic == '':
+        ic = ' '
+
+    return int(n), ic
+
+p = PDB.PDBParser()
+
+argparser = ArgumentParser()
+argparser.add_argument("--pdbmap", metavar="pdb_map", type=str, required=True)
+argparser.add_argument("--ensid", metavar="ens_id", type=str, required=True)
+argparser.add_argument("--pdbfile", metavar="pdb_file", type=str, required=True)
+argparser.add_argument("--alnfile", metavar="aln_file", type=str, required=True)
+argparser.add_argument("--mapfile", metavar="map_file", type=str, required=True)
+argparser.add_argument("--colorfile", metavar="color_file", type=str, required=True)
+
+args = argparser.parse_args()
+
+for l in open(args.pdbmap):
+    f = l.rstrip().split()
+    if f[0] == args.ensid:
+        chain_id = f[2]
+        pdb_begin_id, pdb_begin_ins = parse_coord(f[6])
+        pdb_end_id, pdb_end_ins = parse_coord(f[7])
+        break
+else:
+    raise ValueError("Ensembl ID not found")
+
+omega_map = []
+# Omega   lower   upper   LRT_Stat        Pval    Adj.Pval
+for l in open(args.mapfile):
+    f = l.rstrip().split()
+    omega_map.append(f)
+
+seqs = list(AlignIO.read(args.alnfile, 'fasta'))
+
+pdb_seq = seqs[0]
+ens_seq = seqs[1]
+
+pdb_id = pdb_seq.name
+ens_id = ens_seq.name
+
+pdb = p.get_structure(pdb_id, args.pdbfile)
+chain = list(pdb[0][chain_id])
+
+found_begin_id, found_begin_i, found_begin_id, found_end_i = \
+utils.parse_chain(chain, pdb_begin_id, pdb_begin_ins, pdb_end_id, pdb_end_ins)
+
+chain = chain[found_begin_i:found_end_i+1]
+chain_seq = ''.join([ SeqUtils.seq1(r.resname) for r in chain if SeqUtils.seq1(r.resname) != 'X' ])
+# assert chain_seq == str(pdb_seq.seq.ungap('-'))
+print chain_seq
+print str(pdb_seq.seq.ungap('-'))
+
+residues = []
+for i, r in enumerate(chain):
+    if float(omega_map[i][0]) > 1:
+        residues.append(r['CA'].get_coord())
+
+residues = np.array(residues)
+af = AffinityPropagation().fit(residues)
+labels = af.labels_
+
+palette = brewer2mpl.get_map('Spectral', 'Diverging', max(labels)+1)
+colorfile = open(args.colorfile, 'w')
+for i, l in enumerate(labels):
+    print >>colorfile, '\t'.join(str(it) for it in [i] + palette.colors[l])
--- bin/utils.py
+++ bin/utils.py
@@ -3,6 +3,9 @@ from os import path
 import httplib2
 import urllib
 import json
+from itertools import izip_longest
+
+from Bio import PDB
 
 def check_dir(dirname):
     if not path.exists(dirname):
@@ -32,3 +35,38 @@ def ens_get(ext, *args, **kwargs):
     return decoded
 
 
+def grouper(iterable, n, fillvalue=None):
+    "Collect data into fixed-length chunks or blocks"
+    # grouper('ABCDEFG', 3, 'x') --> ABC DEF Gxx
+    args = [iter(iterable)] * n
+    return izip_longest(fillvalue=fillvalue, *args)
+
+def parse_chain(pdb_chain, pdb_begin_id, pdb_begin_ins, pdb_end_id, pdb_end_ins):
+    pdb_chain = list(pdb_chain)
+
+    for i, r in enumerate(pdb_chain):
+        if r.id[1] == pdb_begin_id and r.id[2] == pdb_begin_ins:
+            found_begin_id = r.id[1]
+            found_begin_i = i
+            break
+        elif r.id[1] > pdb_begin_id:
+            found_begin_id = r.id[1]
+            found_begin_i = i
+            break
+    else:
+        raise IndexError("Residue not found")
+
+    for i, r in reversed(list(enumerate(pdb_chain))):
+        if r.id[1:] == (pdb_end_id, pdb_end_ins):
+            found_end_id = r.id[1]
+            found_end_i = i
+            break
+        # We need to check if the residue is an aa because some chains contain misnumbered HETATMs 
+        elif r.id[1] < pdb_end_id and r.id[0] == ' ' and PDB.is_aa(r):
+            found_end_id = r.id[1]
+            found_end_i = i
+            break
+    else:
+        raise IndexError("Residue not found")
+
+    return found_begin_id, found_begin_i, found_begin_id, found_end_i
