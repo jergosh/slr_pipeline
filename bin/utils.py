84869542bd116cf705adcef51c817970a5df7c1d
Greg Slodkowicz <jergosh@gmail.com>
2015-09-09 16:13:05 +0100

    Committing utils.py for merging.

--- bin/utils.py
+++ bin/utils.py
@@ -4,6 +4,8 @@ import httplib2
 import urllib
 import json
 from itertools import izip_longest
+import re
+import pprint
 
 from Bio import PDB
 
@@ -70,3 +72,49 @@ def parse_chain(pdb_chain, pdb_begin_id, pdb_begin_ins, pdb_end_id, pdb_end_ins)
         raise IndexError("Residue not found")
 
     return found_begin_id, found_begin_i, found_begin_id, found_end_i
+
+ens_re = re.compile("^([A-Z]+)")
+species_file = open("species_codes.txt")
+species_map = {}
+
+for l in species_file:
+    f = l.rstrip().split('\t')
+    species_map[f[0]] = f
+
+# pprint.pprint(species_map)
+def ens2species(ens_id, long_name=False):
+    prefix = ens_re.match(ens_id)
+    if prefix == None:
+        return None
+
+    prefix = prefix[:-1]
+    if prefix in species_map:
+        if long_name:
+            return species_map[prefix][1]
+        else:
+            return species_map[prefix][2]
+    else:
+        return None
+    
+def parse_stk(fh):
+    seqs = {}
+    for l in fh:
+        l = l.rstrip()
+        if l.startswith("#"):
+            continue
+
+        if not len(l):
+            continue 
+
+        if l == "//":
+            break
+
+        parts = [x.strip() for x in l.split(" ", 1)]
+        id, seq = parts
+
+        if id not in seqs:
+            seqs[id] = seq
+        else:
+            seqs[id] += seq
+
+    return seqs
