b60cd23d8585c7f079635f5f74584f25c570d308
Greg Slodkowicz <jergosh@gmail.com>
2015-10-03 15:53:19 +0100

    Added a script for setting up the branch-site tests

---
+++ bin/prepare_branch_site.py
@@ -0,0 +1,35 @@
+import argparse
+from os import path
+import glob
+# from Bio.Phylo.PAML import codeml
+
+# Model A (alternative): model = 2, NSsites = 2,  fix_omega = 0 
+# Model A1 (null): model = 2, NSsites = 2,  fix_omega = 1, omega = 1 
+
+import ete2
+
+argparser = argparse.ArgumentParser()
+
+# Use an ID list + dataset map as a way of limiting the number of files?
+argparser.add_argument('--treedir', metavar="dir", type=str, required=True)
+
+# Alignment stays the same but maybe needs to be copied to a subdir
+# Dir structure:
+# 100 dirs 
+# subdir for each dataset/branch combination, in the form 100_1_2
+def main():
+    args = argparser.parse_args()
+
+    for tree_fn in glob.glob(path.join()):
+        basename = path.basename(tree_fn)
+        dataset = basename.partition('_')[0]
+        prefix = dataset.partition('_')[0][:2]
+
+        tree = ete2.Tree(tree_fn)
+
+        for node in tree.traverse("postorder"):
+            print node.name
+            node.name += "#1"
+
+if __name__ == "__main__":
+    main()
