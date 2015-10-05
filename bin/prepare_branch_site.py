41a64de2e14964400f847c066fbbb71e1ab9ed1b
Greg Slodkowicz <jergosh@gmail.com>
2015-10-05 13:41:21 +0100

    Finishing off the branch-site submission script.

--- bin/prepare_branch_site.py
+++ bin/prepare_branch_site.py
@@ -5,7 +5,8 @@ import glob
 
 # Model A (alternative): model = 2, NSsites = 2,  fix_omega = 0 
 # Model A1 (null): model = 2, NSsites = 2,  fix_omega = 1, omega = 1 
-
+from Bio import AlignIO
+from Bio import SeqIO
 import ete2
 
 import utils
@@ -14,6 +15,7 @@ argparser = argparse.ArgumentParser()
 
 # Use an ID list + dataset map as a way of limiting the number of files?
 argparser.add_argument('--treeroot', metavar="dir", type=str, required=True)
+argparser.add_argument('--alnroot', metavar="dir", type=str, required=True)
 argparser.add_argument('--outroot', metavar="dir", type=str, required=True)
 
 # Alignment stays the same but maybe needs to be copied to a subdir
@@ -31,10 +33,17 @@ def main():
         prefix = dataset.partition('_')[0][:2]
 
         tree = ete2.Tree(tree_fn)
+        aln = AlignIO.read(path.join(args.alnroot, prefix, dataset+"_prank.best.fas"))
+        seqnames = SeqIO.to_dict(aln).keys()
+
         outdir = path.join(args.outroot, prefix)
         dataset_dir = path.join(outdir, dataset)
         utils.check_dir(outdir)
         utils.check_dir(dataset_dir)
+
+        tree.prune(seqnames)
+        utils.write_paml(open(path.join(outdir, dataset+'.paml', 'w'), aln)
+
         for i, node in enumerate(tree.traverse("postorder")):
             old_name = node.name
             if node.name == 'NoName':
