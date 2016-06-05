7e8861751b4be811e4bced45641e72f9adc94adc
Greg Slodkowicz <gregs@ebi.ac.uk>
2016-06-05 11:12:53 +0100

    Added a provision for rerunning jobs in SLR submission script.

--- bin/slr_bsub.py
+++ bin/slr_bsub.py
@@ -5,14 +5,16 @@ from os import path
 import argparse
 import re
 import utils
+import sys
 
-slr_cmd = "/nfs/research2/goldman/gregs/sw/slr-1.4.1/bin/Slr {}"
+slr_cmd = "/nfs/research2/goldman/gregs/sw/slr/bin/Slr {}"
 
 argparser = argparse.ArgumentParser()
 
 argparser.add_argument('--clade', metavar='clade', type=str, required=True)
 argparser.add_argument('--slrroot', metavar='slr_root', type=str, required=True)
 argparser.add_argument('--logdir', metavar='log_dir', type=str, required=True)
+argparser.add_argument('--rerun', action='store_true')
 
 args = argparser.parse_args()
 
@@ -25,10 +27,15 @@ utils.check_dir(path.join(logroot, args.clade))
 for infile in glob(path.join(inroot, args.clade, "*", "*.ctl")):
 # for infile in glob(path.join(inroot, args.clade, "*", "*.ctl"))[:10]:
     basename = path.basename(infile)
+    dataset = basename.partition('.')[0]
 
     logdir = path.join(logroot, args.clade, basename[:2])
 
     utils.check_dir(logdir)
+
+    if args.rerun and path.exists(path.join(path.dirname(infile), dataset+'.res')):
+        print >>sys.stderr, "Skipping", dataset
+        continue
 
     logfile = path.abspath(path.join(logdir, basename.rpartition('.')[0] + '.log'))
     # errfile = path.join(logdir, args.clade, basename + '.err')
