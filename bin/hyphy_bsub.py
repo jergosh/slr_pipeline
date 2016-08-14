e9394571e7c1818b90df997c16dcb7c63e53ecb0
Greg Slodkowicz <jergosh@gmail.com>
2016-08-14 12:27:55 +0200

    Added a command-line argument to specify HYPHY.

--- bin/hyphy_bsub.py
+++ bin/hyphy_bsub.py
@@ -7,6 +7,7 @@ import re
 
 import utils
 
+# Default, can be changed with a command-line argument
 fubar_cmd = "/nfs/research2/goldman/gregs/sw/hyphy/HYPHYMP {}"
 
 argparser = argparse.ArgumentParser()
@@ -14,6 +15,7 @@ argparser.add_argument('--clade', metavar='clade_name', type=str, required=True)
 argparser.add_argument('--inroot', metavar='input_root', type=str, required=True)
 argparser.add_argument('--logdir', metavar='log_dir', type=str, required=True)
 argparser.add_argument('--filter', metavar='filter', type=str, default="*_HYPHY")
+argparser.add_argument('--hyphy', metavar='path', type=str, default=fubar_cmd)
 
 args = argparser.parse_args()
 
@@ -38,4 +40,3 @@ for infile in glob(path.join(inroot, args.clade, '*', args.filter)):
     p = Popen(['bsub', '-R', 'rusage[tmp=512]', '-o'+logfile, fubar])
         
     p.wait()
-    
