29240c1f1447afc2b1184f3cf23cac049191c3d8
Greg Slodkowicz <jergosh@gmail.com>
2016-10-02 22:49:25 +0100

    Now out dir and out root passed separately.

--- bin/run_depth.py
+++ bin/run_depth.py
@@ -11,6 +11,7 @@ argparser.add_argument('--infile', metavar='pdb_file', type=str, required=True)
 argparser.add_argument('--stable_id', metavar='stable_id', type=str, required=True)
 argparser.add_argument('--pdb_id', metavar='pdb_id', type=str, required=True)
 argparser.add_argument('--pdb_chain', metavar='pdb_chain', type=str, required=True)
+argparser.add_argument('--outdir', metavar='out_dir', type=str, required=True)
 argparser.add_argument('--outroot', metavar='out_root', type=str, required=True)
 argparser.add_argument('--logfile', metavar='log_file', type=str, required=True)
 argparser.add_argument('--depth_exec', metavar='bin', type=str,
@@ -20,16 +21,17 @@ argparser.add_argument('--depth_exec', metavar='bin', type=str,
 if __name__ == "__main__":
     args = argparser.parse_args()
 
-    depth_cmd = "{} -i {} -o {}".format(args.depth_exec, args.infile, args.outroot)
+    depth_cmd = "{} -i {} -o {}".format(args.depth_exec, args.infile,
+                                        path.join(args.outdir, args.outroot))
     
     p = subprocess.Popen([ "bsub", "-o"+args.logfile, depth_cmd ])
     p.wait()
 
-    outfile = open(path.join(args.outroot,
+    outfile = open(path.join(args.outdir,
                              "_".join([args.stable_id, args.pdb_id, args.pdb_chain])+'.tab'), 'w')
     print >>outfile, "\t".join(["stable_id", "pdb_id", "pdb_chain", "pdb_pos", "depth"])
 
-    infile = open(args.outroot + "-residue.depth")
+    infile = open(path.join(args.outdir, args.outroot) + "-residue.depth")
     infile.readline()
     for l in infile:
         f = l.rstrip().split('\t')
