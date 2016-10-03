d15e2dfe62b471360b4642e21325938082ef1f05
Greg Slodkowicz <jergosh@gmail.com>
2016-10-03 11:06:52 +0100

    Giving up on bsub for the time being.

--- bin/depth_bsub.py
+++ bin/depth_bsub.py
@@ -22,13 +22,22 @@ if __name__ == "__main__":
     infile.drop_duplicates(subset=["stable_id", "pdb_id", "pdb_chain"], inplace=True)
 
     for i, r in infile.iterrows():
-        p = subprocess.Popen(["bsub",
-                              "-o"+path.join(args.logdir, "_".join([ r['stable_id'], r['pdb_id'], r['pdb_chain'] ])+'.log'),
-                              "python " +
-                              args.depth_cmd +
-                              " --infile " + path.join(args.pdbdir, "pdb"+r['pdb_id']+".ent") +
-                              " --stable_id " + r['stable_id'] +
-                              " --pdb_id " + r['pdb_id'] +
-                              " --pdb_chain " + r['pdb_chain'] +
-                              " --outdir " + args.outdir])
+        # p = subprocess.Popen(["bsub",
+        #                       "-o"+path.join(args.logdir, "_".join([ r['stable_id'], r['pdb_id'], r['pdb_chain'] ])+'.log'),
+        #                       "python " +
+        #                       args.depth_cmd +
+        #                       " --infile " + path.join(args.pdbdir, "pdb"+r['pdb_id']+".ent") +
+        #                       " --stable_id " + r['stable_id'] +
+        #                       " --pdb_id " + r['pdb_id'] +
+        #                       " --pdb_chain " + r['pdb_chain'] +
+        #                       " --outdir " + args.outdir])
+
+        p = subprocess.Popen(["python",
+                              args.depth_cmd,
+                              "--infile", path.join(args.pdbdir, "pdb"+r['pdb_id']+".ent"),
+                              "--stable_id", r['stable_id'],
+                              "--pdb_id", r['pdb_id'],
+                              "--pdb_chain", r['pdb_chain'],
+                              "--outdir", args.outdir])
+
         p.wait()
